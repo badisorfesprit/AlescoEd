2b42a0878bc41aab7b345a71f87003cfcf913f06
nawresmhedhbi <47056839+nawresmhedhbi@users.noreply.github.com>
2019-04-19 01:56:17 +0100

    Add files via upload

---
+++ MapdetailsController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alecso;
+
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import static jdk.nashorn.internal.objects.NativeArray.map;
+
+/**
+ * FXML Controller class
+ *
+ * @author Nawres
+ */
+public class MapdetailsController implements Initializable {
+
+    @FXML
+    private GoogleMapView mapView;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    //mapView.addMapInializedListener(this);
+ 
+    
+    
+
+
+
+    
+}
